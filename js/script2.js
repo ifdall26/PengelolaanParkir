2507fb6ed43ad8f71529543bf5a264f77e09a37a
ifdall26 <lisyukri.ifdal@gmail.com>
2024-03-29 01:38:44 +0700

    membuat method kendaraanMasuk

--- js/script2.js
+++ js/script2.js
@@ -1,8 +1,13 @@
 // membuat objek
-function Parkiran(pengelola, platNomorKendaraan, kas) {
+function Parkiran(pengelola, slotParkir, kas) {
   this.pengelola = pengelola;
-  this.platNomorKendaraan = platNomorKendaraan;
+  this.slotParkir = slotParkir;
   this.kas = kas;
+
+  this.kendaraanMasuk = function (platNomorKendaraan) {
+    this.slotParkir.push(platNomorKendaraan);
+    return this.slotParkir;
+  };
 }
 var parkiranA = new Parkiran("Ifdal", [], 0);
 var parkiranB = new Parkiran("Lisyukri", [], 0);
