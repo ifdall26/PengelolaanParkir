f1b5c4de86eddf6924bd2183566c4c940f5a13ac
ifdall26 <lisyukri.ifdal@gmail.com>
2024-03-29 01:28:34 +0700

    membuat objek

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+// membuat objek
+function Parkiran(pengelola, platNomorKendaraan, kas) {
+  this.pengelola = pengelola;
+  this.platNomorKendaraan = platNomorKendaraan;
+  this.kas = kas;
+}
+var parkiranA = new Parkiran("Ifdal", [], 0);
+var parkiranB = new Parkiran("Lisyukri", [], 0);
