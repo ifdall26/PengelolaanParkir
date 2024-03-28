d8ffaad57fba800cd117ea6b90dc4571722dc2da
ifdall26 <lisyukri.ifdal@gmail.com>
2024-03-29 01:58:54 +0700

    membuat method kendaraanKeluar

--- js/script2.js
+++ js/script2.js
@@ -8,6 +8,21 @@ function Parkiran(pengelola, slotParkir, kas) {
     this.slotParkir.push(platNomorKendaraan);
     return this.slotParkir;
   };
+
+  this.kendaraaanKeluar = function (platNomorKendaraan, tarif) {
+    if (slotParkir.length === 0) {
+      console.log("Seluruh slot parkir dalam keadaan kosong");
+      return false;
+    }
+
+    for (var i = 0; i < slotParkir.length; i++) {
+      if (this.slotParkir[i] == platNomorKendaraan) {
+        this.slotParkir[i] = undefined;
+        this.kas += tarif;
+        return slotParkir;
+      }
+    }
+  };
 }
 var parkiranA = new Parkiran("Ifdal", [], 0);
 var parkiranB = new Parkiran("Lisyukri", [], 0);
